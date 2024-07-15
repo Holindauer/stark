0bbba535bf5b1644514bf29d332b702f23ba2c3f
Hunter Lindauer <holindauer@gmail.com>
2024-07-15 08:53:24 -0700

    added polynomial subtraction

--- src/modules/polynomial.rs
+++ src/modules/polynomial.rs
@@ -70,6 +70,66 @@ impl Add for Polynomial {
     }
 }
 
+// polynomial subtraction
+impl Sub for Polynomial {
+    type Output = Polynomial;
+
+    fn sub(self, rhs: Polynomial) -> Polynomial {
+
+        // init result coeffs vec
+        let mut res: Vec<FieldElement> = vec![];
+        let diff_len: usize;
+
+        let rhs_offset: usize;
+        let lhs_offset: usize;
+
+        let max_poly: Polynomial;
+        let min_poly: Polynomial;
+
+        // if subtrahend is longer, push negated rhs terms until poly degree union 
+        if rhs.coeffs.len() > self.coeffs.len() {
+
+            // set max and min polynomials, and difference in length
+            max_poly = rhs.clone(); min_poly = self.clone();
+            diff_len = max_poly.coeffs.len() - min_poly.coeffs.len();
+            for i in 0..diff_len {res.push(zero() - *rhs.coeffs.get(i).unwrap()); }
+
+            // set offsets for degree union subtraction
+            rhs_offset = diff_len; lhs_offset = 0;
+        }
+        // if minuend is longer, push lhs terms until poly degree union
+        else if self.coeffs.len() > rhs.coeffs.len() {
+
+            // set max and min polynomials, and difference in length
+            max_poly = self.clone(); min_poly = rhs.clone();
+            diff_len = max_poly.coeffs.len() - min_poly.coeffs.len();
+
+            // push lhs terms until poly degree union
+            for i in 0..diff_len {res.push(*max_poly.coeffs.get(i).unwrap()); }
+
+            // set offsets for degree union subtraction
+            rhs_offset = 0; lhs_offset = diff_len;
+        }
+        else { 
+            // max and min polynomials are equal in this case
+            min_poly = rhs.clone();
+            rhs_offset = 0; lhs_offset = 0;
+        }
+
+        // sub remaining terms elementwise
+        let union_len = min_poly.coeffs.len();
+        for i in 0..union_len {
+            let rhs_coeff: FieldElement = *rhs.coeffs.get(rhs_offset+i).unwrap();
+            let lhs_coeff: FieldElement = *self.coeffs.get(lhs_offset+i).unwrap();
+            res.push(lhs_coeff - rhs_coeff);
+        }
+
+        println!("{:?}", res);
+
+        Polynomial::new(res)
+    }
+}
+
 // negation
 impl Neg for Polynomial {
     type Output = Polynomial;
@@ -83,6 +143,8 @@ impl Neg for Polynomial {
         Polynomial::new(res)
     }
 }
+
+
 
 // prints polynomial formatted polynomial
 impl fmt::Display for Polynomial {
@@ -107,16 +169,9 @@ mod tests {
     #[test]
     fn add_same_degree() {
 
-
-        // field element coefficients
-        let coeffs_1: Vec<FieldElement> = vec![new_field_element(10), new_field_element(3), new_field_element(1)];
-        let coeffs_2: Vec<FieldElement> = vec![new_field_element(90), new_field_element(3), new_field_element(1)];
-
-        // polynomials
-        let poly_1: Polynomial = Polynomial::new(coeffs_1);
-        let poly_2: Polynomial = Polynomial::new(coeffs_2);
-
-        // add them
+        // add polynomials
+        let poly_1: Polynomial = Polynomial::new(vec![new_field_element(10), new_field_element(3), new_field_element(1)]);
+        let poly_2: Polynomial = Polynomial::new(vec![new_field_element(90), new_field_element(3), new_field_element(1)]);
         let poly = poly_1.clone() + poly_2.clone();
 
         // ensure correct
@@ -125,6 +180,62 @@ mod tests {
         assert_eq!(poly.coeffs.get(2).unwrap().value, 2);
     }
 
+
+    #[test]
+    fn sub_same_degree() {
+
+        // add polynomials
+        let poly_1: Polynomial = Polynomial::new(vec![new_field_element(80), new_field_element(6), new_field_element(1)]);
+        let poly_2: Polynomial = Polynomial::new(vec![new_field_element(40), new_field_element(3), new_field_element(6)]);
+        let poly = poly_1.clone() - poly_2.clone();
+
+        println!("coeffs len: {}", poly.coeffs.len());
+
+        // ensure correct
+        assert_eq!(poly.coeffs.get(0).unwrap().value, 40);
+        assert_eq!(poly.coeffs.get(1).unwrap().value, 3);
+        assert_eq!(poly.coeffs.get(2).unwrap().value, -5);
+    }
+
+    #[test]
+    fn sub_larger_lhs() {
+
+        // add polynomials
+        let lhs: Polynomial = Polynomial::new(vec![
+            new_field_element(80), new_field_element(6), new_field_element(1), 
+            new_field_element(80), new_field_element(6), new_field_element(1)]);
+        let rhs: Polynomial = Polynomial::new(vec![new_field_element(40), new_field_element(3), new_field_element(6)]);
+        let poly = lhs.clone() - rhs.clone();
+
+        // ensure correct
+        assert_eq!(poly.coeffs.len(), 6);
+        assert_eq!(poly.coeffs.get(0).unwrap().value, 80);
+        assert_eq!(poly.coeffs.get(1).unwrap().value, 6);
+        assert_eq!(poly.coeffs.get(2).unwrap().value, 1);
+        assert_eq!(poly.coeffs.get(3).unwrap().value, 40);
+        assert_eq!(poly.coeffs.get(4).unwrap().value, 3);
+        assert_eq!(poly.coeffs.get(5).unwrap().value, -5);
+    }
+
+    #[test]
+    fn sub_larger_rhs() {
+
+        // add polynomials
+        let lhs: Polynomial = Polynomial::new(vec![new_field_element(40), new_field_element(3), new_field_element(6)]);
+        let rhs: Polynomial = Polynomial::new(vec![
+            new_field_element(80), new_field_element(6), new_field_element(1), 
+            new_field_element(80), new_field_element(6), new_field_element(1)]);
+        let poly = lhs.clone() - rhs.clone();
+
+        // ensure correct
+        assert_eq!(poly.coeffs.len(), 6);
+        assert_eq!(poly.coeffs.get(0).unwrap().value, -80);
+        assert_eq!(poly.coeffs.get(1).unwrap().value, -6);
+        assert_eq!(poly.coeffs.get(2).unwrap().value, -1);
+        assert_eq!(poly.coeffs.get(3).unwrap().value, -40);
+        assert_eq!(poly.coeffs.get(4).unwrap().value, -3);
+        assert_eq!(poly.coeffs.get(5).unwrap().value, 5);
+    }
 
     #[test]
     fn add_diff_degree() {
