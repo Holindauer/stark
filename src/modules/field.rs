e354b1b9f5a55b764aaf374e8fb2ea26f320c416
Hunter Lindauer <holindauer@gmail.com>
2024-07-17 13:03:08 -0700

    modified field to use big int to support hgiher modulus

--- src/modules/field.rs
+++ src/modules/field.rs
@@ -1,206 +1,173 @@
+
 use std::ops::{Add, Sub, Mul, Div, Neg};
 use std::fmt;
-use rand::Rng;
+use num_bigint::RandBigInt;
+use num_bigint::BigInt;
+use num_traits::{Zero, One};
 use serde::{Serialize, Deserialize};
 
-/**
- * @notice field.rs contains a finite field implementation for the modulus 3 * 2^30 + 1
- * It contains a generate with value 5. 
- */
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-pub struct FieldElement{
-    pub value: i128,
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct FieldElement {
+    pub value: BigInt,
 }
 
 impl FieldElement {
-
-    // constructor for field element at value w/ prime mod 3 * 2^30 + 1
-    pub fn new(input_value: i128) -> FieldElement{
-        let value: i128 = input_value % FieldElement::modulus();
-        FieldElement{value}
+    // constructor for field element with value within prime modulus
+    pub fn new(value: BigInt) -> FieldElement {
+        FieldElement { value: value % FieldElement::modulus() }
     }
 
     // generator constructor
-    pub fn generator() -> FieldElement{ FieldElement::new(5) }
+    pub fn generator() -> FieldElement { FieldElement::new(BigInt::from(5)) }
 
-    // modulus 
-    pub fn modulus() -> i128 { 3 * 2_i128.pow(30) + 1 }
+    // prime modulus of 1 + 407 * 2^119
+    pub fn modulus() -> BigInt { BigInt::from(407) * (BigInt::from(2).pow(119)) + 1 }
 
-    // zero and one constructor 
-    pub fn zero() -> FieldElement{ FieldElement::new(0) }
-    pub fn one() -> FieldElement{ FieldElement::new(1) }
+    // zero and one constructors
+    pub fn zero() -> FieldElement { FieldElement::new(BigInt::zero()) }
+    pub fn one() -> FieldElement { FieldElement::new(BigInt::one()) }
 
     // exponentiation
     pub fn pow(&self, exp: i128) -> FieldElement {
-
         let mut base = self.clone();
-        let mut exp = exp.clone();
-        let mut result = FieldElement::one(); 
+        let mut exp = BigInt::from(exp);
+        let mut result = FieldElement::one();
 
-        // exponentiation by repeated squaring
-        while exp > 0 {
-            if exp % 2 == 1 {
-                result = result * base;
+        //  modular exponentiation 
+        while !exp.is_zero() {
+            if &exp % 2 == BigInt::from(1) {
+                result = result * base.clone();
             }
-            base = base * base;
+            base = base.clone() * base;
             exp /= 2;
         }
-        result 
+        result
     }
 
     // random field element
     pub fn random(&self) -> FieldElement {
         let mut rng = rand::thread_rng();
-        FieldElement::new(rng.gen_range(0..3221225472))
+        let upper_bound = FieldElement::modulus();
+        let random_value = rng.gen_bigint_range(&BigInt::zero(), &upper_bound);
+        FieldElement::new(random_value)
     }
 
-    // multiplicative inverse
+    // multiplicative inverse using the extended Euclidean algorithm
     pub fn inverse(&self) -> FieldElement {
-        let mut t = 0;
-        let mut new_t = 1;
+        let zero = BigInt::zero();
+        let one = BigInt::one();
+        let mut t = zero.clone();
+        let mut new_t = one.clone();
         let mut r = FieldElement::modulus();
-        let mut new_r = self.value;
-        
-        while new_r != 0 {
-            let quotient = r / new_r;
-            (t, new_t) = (new_t, t - quotient * new_t);
-            (r, new_r) = (new_r, r - quotient * new_r);
+        let mut new_r = self.value.clone();
+
+        while new_r != zero {
+            let quotient = &r / &new_r;
+
+            let temp_t = t.clone();
+            t = new_t.clone();
+            new_t = temp_t - &quotient * &new_t;
+
+            let temp_r = r.clone();
+            r = new_r.clone();
+            new_r = temp_r - quotient * new_r;
         }
 
-        if r > 1 { // This means `element.value` and `element.modulus` are not coprime
+        if r > one {
             panic!("Element does not have an inverse");
         }
 
-        // Ensure the result is positive
-        if t < 0 {
-            t += FieldElement::modulus();
+        // Adjust negative result to be positive
+        if t < zero {
+            t = t + FieldElement::modulus();
         }
 
-        FieldElement { value: t }
+        FieldElement::new(t)
     }
 }
 
-// addition
+// Note: modulo applied in FieldElement constructor
 impl Add for FieldElement {
-    type Output = FieldElement;
-
-    // add then mod by prime
-    fn add(self, other: FieldElement) -> FieldElement {
-        FieldElement{
-            value: (self.value + other.value) % FieldElement::modulus(),}
-    }
+    type Output = Self;
+    fn add(self, other: Self) -> Self { FieldElement::new(self.value + other.value) }
 }
 
-// subtraction
 impl Sub for FieldElement {
-    type Output = FieldElement;
+    type Output = Self;
 
-    // subtract then mod by prime
-    fn sub(self, other: FieldElement) -> FieldElement {
-        let mut result = (self.value - other.value) % FieldElement::modulus();
-        if result < 0 {
-            result += FieldElement::modulus();
-        }
-        FieldElement { value: result  }
+    fn sub(self, other: Self) -> Self {
+        FieldElement::new(  
+            // Ensure the result is positive by adding the modulus before taking the modulo
+            (self.value - other.value + FieldElement::modulus()) % FieldElement::modulus() 
+        )
     }
 }
 
-// negation
-impl Neg for FieldElement {
-    type Output = FieldElement;
-
-    fn neg(self) -> FieldElement {
-        let mut result = FieldElement::modulus() - self.value;
-        if result >= FieldElement::modulus() {
-            result -= FieldElement::modulus();
-        }
-        FieldElement { value: result }
-    }
-}
-
-// multiplication
 impl Mul for FieldElement {
-    type Output = FieldElement;
-
-    // multiply then mod by prime
-    fn mul(self, other: FieldElement) -> FieldElement {
-        FieldElement{ value: (self.value * other.value) % FieldElement::modulus() }
-    }
+    type Output = Self;
+    fn mul(self, other: Self) -> Self { FieldElement::new(self.value * other.value) }
 }
 
-// division
 impl Div for FieldElement {
-    type Output = FieldElement;
-
-    // multiply by inverse then mod by prime
-    fn div(self, other: FieldElement) -> FieldElement {
-        self * other.inverse()
-    }
+    type Output = Self;
+    fn div(self, other: Self) -> Self { self * other.inverse() }
 }
 
-// prints field element value
+impl Neg for FieldElement {
+    type Output = Self;
+    fn neg(self) -> Self { FieldElement::new(-self.value) }
+}
+
 impl fmt::Display for FieldElement {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Simple modulo to ensure it's within range and non-negative
-        let value = (self.value % FieldElement::modulus() + FieldElement::modulus()) % FieldElement::modulus();
-        write!(f, "{}", value)
+        write!(f, "{}", self.value)
     }
 }
 
-// tests
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num_bigint::ToBigInt;
     use rand::Rng;
 
     #[test]
     fn addition() {
-        let a = FieldElement::new(3221225472);
-        let b = FieldElement::new(10);
-        assert_eq!(a + b, FieldElement::new(9));
+        let a = FieldElement::new(FieldElement::modulus() - 1.to_bigint().unwrap());
+        let b = FieldElement::new(10.to_bigint().unwrap());
+        assert_eq!(a + b, FieldElement::new(9.to_bigint().unwrap()));
     }
 
     #[test]
     fn subtraction() {
-
-        let a = FieldElement::new(3221225472);
-        let b = FieldElement::new(10);
+        let a = FieldElement::new(FieldElement::modulus() - 1.to_bigint().unwrap());
+        let b = FieldElement::new(10.to_bigint().unwrap());
         let c = a - b;
+
         println!("{:?}", c);
 
-        assert_eq!(3221225472 - 10, c.value);
+        assert_eq!(c, FieldElement::new((FieldElement::modulus() - 11.to_bigint().unwrap()) % FieldElement::modulus()));
     }
 
     #[test]
-    fn inverse_test_fuzz() { 
-
-        // get rand int within mod
-        let mut rng = rand::thread_rng();
-        let random_number = rng.gen_range(1..3221225472); 
-    
-        // ensure multiplicative inverse works
-        let a = -FieldElement::new(random_number);
-        let c  = a.inverse();
-        assert_eq!(c * a, FieldElement::one());
+    fn test_inverse() {
+        let elem = FieldElement::new(3.to_bigint().unwrap());
+        let inv_elem = elem.inverse();
+        let one_elem = FieldElement::new(1.to_bigint().unwrap());
+        assert_eq!(elem * inv_elem, one_elem, "Inverse test failed");
     }
 
     #[test]
-    fn invers_test_2() {
-        // Test with various non-zero elements
-        let test_values = vec![1, 2, 3, 5, 1234567, 3221225470]; // Various values, including near the modulus
+    fn inverse_test_2() {
+        let test_values: Vec<i128> = vec![1, 2, 3, 5, 1234567, 3221225470];
         for val in test_values {
-            let elem = FieldElement::new(val);
+            let elem = FieldElement::new(val.to_bigint().unwrap());
             let inv = elem.inverse();
             let product = elem * inv;
 
-            // Check if the product is 1 (multiplicative identity)
             assert_eq!(product, FieldElement::one(), "Failed inverse test for value: {}", val);
         }
 
-        // Optional: Test edge case for zero, which should not have an inverse
-        // Depending on how you want to handle this, this part might be different
         let zero_elem = FieldElement::zero();
         let result = std::panic::catch_unwind(|| zero_elem.inverse());
         assert!(result.is_err(), "Inverse of zero did not panic as expected");
@@ -208,16 +175,18 @@ mod tests {
 
     #[test]
     fn test_pow() {
-        let a = FieldElement::new(2);
-        assert_eq!(a.pow(32).value, 2_i128.pow(32) % 3221225473);
+        let a = FieldElement::new(2.to_bigint().unwrap());
+        let expected_value = (2.to_bigint().unwrap().pow(32) % FieldElement::modulus());
+        assert_eq!(a.pow(32).value, expected_value);
     }
-
 
     #[test]
     fn test_negative_handling() {
-        let elem1 = FieldElement::new(2);
-        let elem2 = FieldElement::new(3);
-        assert_eq!((elem1 - elem2).value, FieldElement::modulus() - 1);
+        let elem1 = FieldElement::new(2.to_bigint().unwrap());
+        let elem2 = FieldElement::new(3.to_bigint().unwrap());
+        let result = elem1 - elem2;
+        let expected_value = (FieldElement::modulus() - 1.to_bigint().unwrap()) % FieldElement::modulus();
+        assert_eq!(result.value, expected_value);
     }
-
 }
+
