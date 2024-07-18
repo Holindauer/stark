14a0f3209bf86161f0288492d616fe484c78bec2
Hunter Lindauer <holindauer@gmail.com>
2024-07-18 09:11:40 -0700

    added variables func that returns linear parent func for each term in multivariate poly

--- src/modules/multivariate_poly.rs
+++ src/modules/multivariate_poly.rs
@@ -6,36 +6,37 @@ use num_bigint::BigInt;
 use num_traits::{Zero, One};
 use std::ops::{Add, Sub, Mul, Neg};
 
+/**
+    Multivariate polynomials are represented as dictionaries with exponents
+    vectors as keys and coefficients as values.
+
+    f(x,y,z) = 17 + 2xy + 42z - 19x^6*y^3*z^12 is represented as:
+        
+    {
+        (0,0,0) => 17,
+        (1,1,0) => 2,
+        (0,0,1) => 42,
+        (6,3,12) => -19,
+    }
+*/
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct MPolynomial {
-    /**
-        Multivariate polynomials are represented as dictionaries with exponents
-        vectors as keys and coefficients as values.
-
-        f(x,y,z) = 17 + 2xy + 42z - 19x^6*y^3*z^12 is represented as:
-        {
-            (0,0,0) => 17,
-            (1,1,0) => 2,
-            (0,0,1) => 42,
-            (6,3,12) => -19,
-        }
-     */
-
     pub dict: HashMap<Vec<i128>, FieldElement>,
 }
 
-
 impl MPolynomial {
 
-    pub fn zero() -> MPolynomial {
-        MPolynomial { dict: HashMap::new() }
+    // constructors
+    pub fn new(dict: HashMap<Vec<i128>, FieldElement>) -> MPolynomial { MPolynomial { dict } }
+    pub fn zero() -> MPolynomial { MPolynomial { dict: HashMap::new() } }
+    pub fn constant(element: i128) -> MPolynomial { 
+        MPolynomial { dict: HashMap::from([(vec![0], FieldElement::new(BigInt::from(element)))]) } 
     }
 
-    pub fn is_zero(&self) -> bool {
-        self.dict.values().all(FieldElement::is_zero)
-    }
+    // checks
+    pub fn is_zero(&self) -> bool { self.dict.values().all(FieldElement::is_zero) }
 
-    // exponentiation by squaring
+    // efficient exponentiation by squaring
     pub fn pow(self, exponent: usize) -> Self {
         if self.is_zero() { return MPolynomial::zero(); } // 0^n = 0
 
@@ -54,13 +55,34 @@ impl MPolynomial {
         acc
     }
 
+    /// Returns a vector of multivariate polynomials representing each indeterminate's linear 
+    /// function with a leading coefficient of one. For example, for three indeterminates, it 
+    /// returns: [f(x,y,z) = x, f(x,y,z) = y, f(x,y,z) = z]
+    pub fn variables(num_variables: usize) -> Vec<Self> {
+        let mut variables = Vec::new();
+        for i in 0..num_variables {
+
+            // Create a dictionary with a single term for each variable
+            let mut exponent = vec![0; num_variables];
+
+            // package the exponent 1 in to the dict at the index i 
+            exponent[i] = 1;
+            let coefficient = FieldElement::one();
+            let mut dict = HashMap::new();
+            dict.insert(exponent, coefficient);
+
+            // push the polynomial to the vector
+            variables.push(MPolynomial { dict });
+        }
+        variables
+    }
+
 
 }
 
-
+// multivariate poly addition
 impl Add for MPolynomial {
     type Output = MPolynomial;
-
 
     fn add(self, rhs: Self) -> Self::Output {
 
@@ -83,6 +105,7 @@ impl Add for MPolynomial {
     }
 }
 
+// multivariate poly subtraction
 impl Sub for MPolynomial {
     type Output = MPolynomial;
 
@@ -91,7 +114,7 @@ impl Sub for MPolynomial {
     }
 }
 
-
+// multivariate poly negation
 impl Neg for MPolynomial {
     type Output = MPolynomial;
 
@@ -107,7 +130,7 @@ impl Neg for MPolynomial {
     }
 }
 
-
+// multivariate poly multiplication
 impl Mul for MPolynomial {
     type Output = Self;
 
